699c3809af4d2e3a79e21ae65777ac18e0d0d7b6
youngjjje <1443009@a.ut.ac.kr>
2025-05-28 00:25:58 +0900

    1.11 initialization add social login

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
-import {useState } from "react";
+import {useEffect, useState } from "react";
 import AppRouter from "components/Router";
 import {authService} from "fbase";
 
 function App() {
+  const [init, setInit] = useState(false)
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser); // eslint-disable-line no-unused-vars
-
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+    if (user) {
+      setIsLoggedIn(user)
+    } else {
+      setIsLoggedIn(false)
+    }
+  setInit(true)
+  });
+  }, [])
   return (
     <>
       <AppRouter isLoggedIn = {isLoggedIn} />
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,13 @@
+// 로그인 / 회원가입
+
+import {authService, firebaseInstance} from "fbase";
 import {useState} from "react";
 
 const Auth = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [newAccount, setNewAccount] = useState(true)
+    const [error, setError] = useState("") // 에러 메시지
 
     const onChange = (event) => {
         const {
@@ -14,20 +19,58 @@ const Auth = () => {
             setPassword(value)
         }
     };
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault();
+        try {
+            let data;
+            if (newAccount){
+                // create newAccount
+                data = await authService.createUserWithEmailAndPassword(email, password);
+            } else {
+                // log in
+                data = await authService.signInWithEmailAndPassword(email, password);
+            }
+            console.log(data)
+        } catch (error){
+            setError(error.message)
+        }
+        
     }
 
+    const toggleAccount = () => setNewAccount((prev) => !prev)
+
+    const onSocialClick = async (event) => {
+        const {
+            target: {name},
+        } = event
+        let provider
+        if(name === "google"){
+            provider = new firebaseInstance.auth.GoogleAuthProvider();
+        }else if (name === "github"){
+            provider = new firebaseInstance.auth.GithubAuthProvider();
+        }
+        
+        const data = await authService.signInWithPopup(provider)
+        console.log(data)
+    }
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange}/>
                 <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange}/>
-                <input type="submit" value="Log In"/>
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+                {error}
             </form>
+            <span onClick={toggleAccount}>
+                {newAccount ? "Sign In" : "Create Account"}
+            </span>
             <div>
-                <button>Contine with Google</button>
-                <button>Contine with Github</button>
+                <button onClick={onSocialClick} name="google">
+                    Contine with Google
+                </button>
+                <button onClick={onSocialClick} name="github">
+                    Contine with Github
+                </button>
             </div>
         </div>
     )
