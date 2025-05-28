84a6f9962c1e7db7c04800e5723511d5dbcef498
youngjjje <1443009@a.ut.ac.kr>
2025-05-28 17:01:22 +0900

    1.12 add Logout

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,24 @@
-import {HashRouter as Router, Route, Routes } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import {HashRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile"
+import Navigation from "./Navigation";
 
 const AppRouter = ({isLoggedIn}) => {
     return (
         <Router>
+            {isLoggedIn && <Navigation />}
             <Routes>
-                { isLoggedIn ?(
-                    <Route exact path="/" element={<Home />}></Route>
+                { isLoggedIn ? (
+                    <>
+                        <Route exact path="/" element= {<Home />} />
+                        <Route exact path="/profile" element={<Profile />} />
+                    </>
                 ) : (
-                    <Route exact path="/" element={<Auth />}></Route>
+                    <>
+                        <Route exact path="/" element={<Auth />}/>
+                        <Route path="*" element={<Navigate to="/" />} />
+                    </>
                 )}
             </Routes>
         </Router>
