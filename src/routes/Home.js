f04383fb173647054b1438d587931803ba7ad937
youngjjje <1443009@a.ut.ac.kr>
2025-06-11 11:48:35 +0900

    1.14 CRUD update delete

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,21 +1,20 @@
 import {dbService} from "fbase";
 import {useEffect, useState} from "react";
+import Nweet from "components/Nweet";
 
 const Home = ({userObj}) => {
     const [nweet, setNweet] = useState("")
     const [nweets, setNweets] = useState([])
 
-    const getNweets = async () => {
-        const dbNweets = await dbService.collection("nweets").get()
-
-        dbNweets.forEach((document) => {
-            const nweetObject = { ...document.data(), id: document.id}
-            setNweets((prev) => [nweetObject, ...prev])
-        })
-    }
-
     useEffect(() => {
-        getNweets()
+        dbService.collection("nweets").onSnapshot((snapshot) => {
+            const newArray = snapshot.docs.map((document) => ({
+                id: document.id,
+                ...document.data(),
+            }))
+            setNweets(newArray)
+
+        })
     }, [])
 
 
@@ -52,9 +51,8 @@ const Home = ({userObj}) => {
             </form>
             <div>
                 {nweets.map((nweet)=> (
-                    <div key={nweet.id}>
-                        <h4>{nweet.text}</h4>
-                    </div>
+                    <Nweet key={nweet.id} nweetObj={nweet} isOwner={nweet.createId === userObj.uid}/>
+
                 ))}
             </div>
         </>
